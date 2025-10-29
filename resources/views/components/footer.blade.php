b5ea951e8b8e8a1c8ecba4782f4a17f63ac81dd2
martijn07 <dmb0070@edu.curio.nl>
2025-10-29 14:06:11 +0100

    ticket #4
    there has been social links about us and contact informatie added to the footer i made this horizantol for better feel of the page

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,39 @@
 
-<footer class="bg-dark text-white p-3 mt-4">
-	<div class="container">
-		© {{ __('misc.copyright') }}
-	</div>
+<footer class="site-footer bg-dark text-white mt-4">
+  <div class="footer-top py-5">
+    <div class="container">
+      <div class="row">
+  <div class="col-6 col-md-4 mb-3">
+          <h5>Over ons</h5>
+          <p class="mb-0">Wij zijn een team dat gebruikers helpt hun gebruiksaanwijzingen eenvoudig te vinden en te downloaden.</p>
+        </div>
+
+  <div class="col-6 col-md-4 mb-3">
+          <h5>Contact</h5>
+          <ul class="list-unstyled mb-0">
+            <li>Adres: Voorbeeldstraat 1, 1000 AA Stad</li>
+            <li>Tel: +31 20 123 4567</li>
+            <li>Email: info@example.com</li>
+          </ul>
+        </div>
+
+  <div class="col-6 col-md-4 mb-3">
+          <h5>Volg ons</h5>
+          <ul class="list-inline mb-0">
+            <li class="list-inline-item"><a class="text-white" href="#">Facebook</a></li>
+            <li class="list-inline-item"><a class="text-white" href="#">Twitter</a></li>
+            <li class="list-inline-item"><a class="text-white" href="#">Instagram</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer-bottom py-3">
+    <div class="container text-center">
+      © {{ __('misc.copyright') }}
+    </div>
+  </div>
 </footer>
 
 
